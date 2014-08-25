0848851c3e2724266dcf6af1bd013798e6c0d8ad
thebmo <bmosier@gmail.com>
2014-08-25 18:31:15 -0400

    updates!

--- pollsite/nes/views.py
+++ pollsite/nes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 # Create your views here.
-#testestse
+
 # class IndexView(generic.ListView):
     # template_name= 'nes/index.html'
 def index(request):
