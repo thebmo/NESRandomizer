692a35d8be49d3e6d0cf009a629e65477a2fccc4
thebmo <bmosier@gmail.com>
2014-09-09 17:58:26 -0400

    made the Beaten filter option functional.

--- pollsite/nes/templatetags/nes_extras.py
+++ pollsite/nes/templatetags/nes_extras.py
@@ -18,10 +18,7 @@ def get_item(dictionary, key):
 def filter_games(all_games, params, games_owned):
     
     games = list(all_games)
-    print '\nall games:'
-    for game in games:
-        print game.title
-        
+
     # removes games that user does not own if an argument
     if params['selection'] == 'owned':
         
@@ -36,13 +33,30 @@ def filter_games(all_games, params, games_owned):
 
     # removes games that don't match the genre if selected
     if params['genres']:
+        
+        # start testing crap
+        print '\nPARAMS:'
+        for param in params['genres']:
+            print param
+        print '\n'
+        # end testing crap
+        
         for game in all_games:
-            if game.genre not in params['genres']:
+            print '%s: %s' % (game.title, game.genre.lower())
+            if game in games and game.genre.lower() not in params['genres']:
                 games.remove(game)
+                # print 'removing: %s' % game.title
     
-    # checks if user set the beaten filter
+    # # checks if user set the beaten filter
     if params['beaten'] != 'both':
+        beaten = (False if params['beaten']  == 'yes' else True)
+        # if params['beaten'] == 'yes':
+        for game in all_games:
+            for owned in games_owned:
+                if game in games and game.id == owned.game_id and owned.beaten == beaten:
+                    games.remove(game)
     
     
     
-    return games if games else None
+    # return (games if games else None)
+    return games
--- pollsite/nes/views.py
+++ pollsite/nes/views.py
@@ -16,6 +16,9 @@ def index(request):
 
 def random_game(request):
     template_name = 'nes/random.html'
+    errors = []
+    debug = True
+    # debug = False
     
     if 'selection' in request.GET:       
         params = {        
@@ -24,16 +27,21 @@ def random_game(request):
             'genres' : request.GET.getlist('genre'),
             }
         all_games = Game.objects.all()
+        games_owned = {}
+        game_url=''
         
         
         # sets the games owned argument for NES.filter_games()
-        games_owned = {}
         if params['selection'] == 'owned':
             games_owned = OwnedGame.objects.filter(user_id=request.user.id)
             
-        random_game = random.choice(NES.filter_games(all_games, params, games_owned))
+        
+        random_game = NES.filter_games(all_games, params, games_owned)
+        if random_game:
+            random_game = random.choice(random_game)
+            game_url= str(random_game.title).replace(' ','+')
+        else:
+            errors.append('No games match this criteria. Remove some filters!')
 
-        game_url= str(random_game.title).replace(' ','+')
-      
-        return render(request, template_name, {'game':random_game, 'selection':params['selection'], 'beaten':params['beaten'], 'genres':params['genres'], 'game_url':game_url})
+        return render(request, template_name, {'game':random_game, 'selection':params['selection'], 'beaten':params['beaten'], 'genres':params['genres'], 'game_url':game_url, 'errors':errors, 'debug':debug})
     return render(request, template_name,)
