298f2943cf53e96e6ccc1e8680853c9bf34c97a7
thebmo <bmosier@gmail.com>
2014-09-17 15:54:12 -0400

    edited title block

--- nes_randomizer/nes/models.py
+++ nes_randomizer/nes/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    publisher = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
+    year = votes = models.IntegerField(default=0)
+    region = models.CharField(max_length=200)
+    format = models.CharField(max_length=200)
+    license = models.CharField(max_length=200)
     rating = models.FloatField(default=0)
     votes = models.IntegerField(default=0)
+    
     def __unicode__(self):
         return self.title
     class Meta:
