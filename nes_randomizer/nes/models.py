f89fb9d7c4796714bbf36f3b0d447af47cba8dc3
thebmo <bmosier@gmail.com>
2014-11-06 23:14:37 -0500

    conforming to pep8 standards

--- nes_randomizer/nes/models.py
+++ nes_randomizer/nes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -12,25 +13,27 @@ class Game(models.Model):
     license = models.CharField(max_length=200)
     rating = models.FloatField(default=0)
     votes = models.IntegerField(default=0)
-    
+
     def __unicode__(self):
         return self.title
+
     class Meta:
         ordering = ('title',)
-    
-class BeatenGame(models.Model):
-    game=models.ForeignKey(Game)
-    user=models.ForeignKey(User)
 
-    
+
+class BeatenGame(models.Model):
+    game = models.ForeignKey(Game)
+    user = models.ForeignKey(User)
+
     class Meta:
         ordering = ('game',)
         unique_together = ['game', 'user']
 
+
 class OwnedGame(models.Model):
-    game=models.ForeignKey(Game)
-    user=models.ForeignKey(User)
-    
+    game = models.ForeignKey(Game)
+    user = models.ForeignKey(User)
+
     class Meta:
         ordering = ('game',)
         unique_together = ['game', 'user']
--- nes_randomizer/nes/views.py
+++ nes_randomizer/nes/views.py
@@ -5,102 +5,98 @@ import random
 from . import forms
 from .templatetags import nes_extras as NES
 
+
 # Create your views here.
-
-# class IndexView(generic.ListView):
-    # template = 'nes/index.html'
-
 def game_details(request, game_id):
     template = 'nes/game_details.html'
-    game = Game.objects.get(id = game_id)
+    game = Game.objects.get(id=game_id)
     game_url = NES.create_google_url(game)
     game_search = NES.create_search_string(game)
     amazon_game = NES.fetch_from_amazon(game)
     desc = NES.fetch_game_html(game)
     videos = NES.fetch_game_videos(game)
-    return render(request, template, { 'game':game, 'game_url':game_url, 'game_search':game_search, 'amazon_game':amazon_game,'desc':desc, 'videos':videos })
+    return render(request, template, {'game': game, 'game_url': game_url, 'game_search': game_search, 'amazon_game': amazon_game, 'desc': desc, 'videos': videos})
+
 
 def search_games(request):
     template = 'nes/search_games.html'
     errors = []
-    
+
     # the search box
     if request.POST:
         games = []
 
         # title search
-        G = Game.objects.filter(title__icontains = request.POST['q'])
+        G = Game.objects.filter(title__icontains=request.POST['q'])
         for g in G:
             if g not in games:
                 games.append(g)
 
         # genre search
-        G = Game.objects.filter(genre__icontains = request.POST['q'])
+        G = Game.objects.filter(genre__icontains=request.POST['q'])
         for g in G:
             if g not in games:
                 games.append(g)
 
         # publisher search
-        G = Game.objects.filter(publisher__icontains = request.POST['q'])
+        G = Game.objects.filter(publisher__icontains=request.POST['q'])
         for g in G:
             if g not in games:
-                games.append(g) 
-        
+                games.append(g)
+
         # year search
-        G = Game.objects.filter(year__icontains = request.POST['q'])
+        G = Game.objects.filter(year__icontains=request.POST['q'])
         for g in G:
             if g not in games:
-                games.append(g) 
-        
-        
+                games.append(g)
+
         if not games:
             errors.append('No games found that match this search.')
-    else:    
+    else:
         games = Game.objects.all()
-    return render(request, template, { 'errors':errors, 'games':games })
+    return render(request, template, {'errors': errors, 'games': games})
 
 
-# the index view    
+# the index view
 def index(request):
     template = 'nes/index.html'
     return render(request, template,)
 
-    
-# The random game generator view    
+
+# The random game generator view
 def random_game(request):
     template = 'nes/random.html'
     errors = []
     # debug = True
     debug = False
-    
-    if 'selection' in request.POST:       
-        params = {        
-            'selection' : request.POST['selection'],
-            'beaten' : request.POST['beaten'],
-            'genres' : request.POST.getlist('genre'),
+
+    if 'selection' in request.POST:
+        params = {
+            'selection': request.POST['selection'],
+            'beaten': request.POST['beaten'],
+            'genres': request.POST.getlist('genre'),
             }
         all_games = Game.objects.all()
         games_owned = {}
         beaten_games = {}
-        game_url=''
-        game_search= ''
-        
+        game_url = ''
+        game_search = ''
+
         # sets the games owned argument for NES.filter_games()
         if params['selection'] == 'owned':
             games_owned = OwnedGame.objects.filter(user_id=request.user.id)
-        
+
         if params['beaten'] != 'both':
             beaten_games = BeatenGame.objects.filter(user_id=request.user.id)
-            
-        
+
         random_game = NES.filter_games(all_games, params, games_owned, beaten_games)
         if random_game:
             random_game = random.choice(random_game)
             game_url = NES.create_google_url(random_game)
             game_search = NES.create_search_string(random_game)
-            
+
         else:
             errors.append('No games match this criteria. Remove some filters!')
 
-        return render(request, template, {'game':random_game, 'selection':params['selection'], 'beaten':params['beaten'], 'genres':params['genres'], 'game_url':game_url, 'errors':errors, 'debug':debug})
+        return render(request, template, {'game': random_game, 'selection': params['selection'], 'beaten': params['beaten'], 'genres': params['genres'], 'game_url': game_url, 'errors': errors, 'debug': debug})
     return render(request, template,)
