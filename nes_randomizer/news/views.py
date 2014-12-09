87d96aaffa505815e4c79788ddd7abba9e9a634e
thebmo <bmosier@gmail.com>
2014-12-09 14:26:55 -0500

    conformed to pep8 standards (created dict variables to pass to templates)

--- nes_randomizer/news/views.py
+++ nes_randomizer/news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import NewsPost
 
+
 # Lists out all posts in descending order
 def index(request):
     template = 'news/index.html'
     posts = list(NewsPost.objects.all())
     posts.reverse()
-    
-    return render(request, template, {'posts': posts})
+    template_vars = {
+        'posts': posts
+        }
+
+    return render(request, template, template_vars)
