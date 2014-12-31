a736da5f552ac834d114e182570e282180b7ccbe
thebmo <bmosier@gmail.com>
2014-12-30 19:46:13 -0500

    added the wysiwyg and updated some CSS

---
+++ nes_randomizer/static/mysite/ckeditor/build-config.js
@@ -0,0 +1,69 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/e45dbbfc639a7e80cf25a7281828fc61
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/e45dbbfc639a7e80cf25a7281828fc61
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	preset: 'basic',
+	ignore: [
+		'.bender',
+		'bender.js',
+		'bender-err.log',
+		'bender-out.log',
+		'dev',
+		'.DS_Store',
+		'.gitattributes',
+		'.gitignore',
+		'gruntfile.js',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'node_modules',
+		'package.json',
+		'README.md',
+		'tests'
+	],
+	plugins : {
+		'basicstyles' : 1,
+		'clipboard' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'sourcearea' : 1,
+		'sourcedialog' : 1,
+		'table' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
