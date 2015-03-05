f4e19fbbc61292b9b752cd2539ca15f78bffa8ed
thebmo <bmosier@gmail.com>
2015-03-05 15:13:56 -0500

    broke up some functions into smaller helper functions

--- nes_randomizer/reports/templatetags/reports_extras.py
+++ nes_randomizer/reports/templatetags/reports_extras.py
@@ -2,6 +2,31 @@ from collections import Counter
 from nes.models import *
 from nes.templatetags.nes_extras import get_genres
 
+# Counts the games and returns a list if count
+# [0 title/string - 1 count - 2 game id/pk]
+# Else returns a dict 'game_id': count
+def game_count(games, count=0):
+        total = {}
+        top = []
+        
+        for game in games:
+            if str(game.game_id) not in total:
+                total[str(game.game_id)] = 1
+            else:
+                total[str(game.game_id)] += 1
+        
+        if count:
+            c = Counter(total)
+            c = c.most_common()[:count] # <<<<< x is a int value
+
+            for item in c:
+                game = Game.objects.get(pk=int(item[0]))
+                top.append([game.title, item[1], int(item[0])])
+                
+            return top
+        else:
+            return total
+
 
 # Gets the total number of games beaten/owned from all users
 def total_games(beaten=False, owned=False):
@@ -17,37 +42,26 @@ def total_games(beaten=False, owned=False):
 
 
 # Returns a list of top 10 or x games beaten or owned
-def fetch_most(count=10, beaten=False, owned=False, genre=False):
+def fetch_most(count=10, beaten=False, owned=False, genre=False, all=False):
     # 0 title/string - 1 count - 2 game id/pk
     top = []
-    
-    # dict of tallied games owned
-    # 'gameid': tally_count
-    total = {}
     
     # Chooses the correct operation based on params
     if beaten:
         games = BeatenGame.objects.all()
     elif owned:
         games = OwnedGame.objects.all()
+    
     else:
         return []
     
+    if all:
+        return game_count(games)
     
     # Handles the top <count> games
     if (beaten or owned) and not genre:
-        for game in games:
-            if str(game.game_id) not in total:
-                total[str(game.game_id)] = 1
-            else:
-                total[str(game.game_id)] += 1
+         top = game_count(games, count)
 
-        c = Counter(total)
-        c = c.most_common()[:count] # <<<<< x is a int value
-
-        for item in c:
-            game = Game.objects.get(pk=int(item[0]))
-            top.append([game.title, item[1], int(item[0])])
     
     # Handles the Genre reports
     if genre:
